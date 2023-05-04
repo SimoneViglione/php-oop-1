732d6e74864af0ac5ebbb925a8342f431706bb8f
Simone Viglione <zimoviglio51@gmail.com>
2023-05-04 15:13:14 +0200

    aggiunti construct e method

--- index.php
+++ index.php
@@ -2,25 +2,43 @@
 
     class Movie {
 
+         public $title;
         public $genre;
         public $length;
         public $date;
+        public $vote1;
+        public $vote2;
+        public $vote3;
+
+        public function __construct($title) {
+            $this->title = $title;
+        }
+
+        public function getAverageVote() {
+            return  ($this->vote1 + $this->vote2 + $this->vote3) / 3;
+        }
 
     }
 
-    $kungfupanda = new Movie();
+    $kungfupanda = new Movie("Kung Fu Panda");
     
     $kungfupanda->genre = "comedy";
     $kungfupanda->length = "92 min";
     $kungfupanda->date = "2008";
+    $kungfupanda->vote1 = 5;
+    $kungfupanda->vote2 = 3;
+    $kungfupanda->vote3 = 4;
 
     var_dump($kungfupanda);
 
-    $scream = new Movie();
+    $scream = new Movie("Scream");
 
     $scream->genre = "horror";
-    $scream->genre = "111 min";
-    $scream->genre = "1996";
+    $scream->length = "111 min";
+    $scream->date = "1996";
+    $scream->vote1 = 4;
+    $scream->vote2 = 2;
+    $scream->vote3 = 5;
 
     var_dump($scream);
 ?>
